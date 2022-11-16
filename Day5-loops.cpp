e6cbfd463179e924c6512178b4582cd5138ffc1f
shreya1881 <2704shreya@gmail.com>
2022-11-16 19:10:13 +0530

    day 5

---
+++ Day5-loops.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    scanf("%d",&n);
+    int i,s;
+    for(i=1; i<=10; i++)
+    {
+       s=n*i;
+       printf("%d x %d = %d\n",n,i,s);
+        
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
